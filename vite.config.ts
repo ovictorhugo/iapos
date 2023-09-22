09915ffedbac023e5a1f81221f251dbb2397a5c1
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2023-09-22 13:38:12 -0300

    versãp com https

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-  server: {
-    host: true,
-    https: true
-  }
+  server: { https: true },
+  plugins: [ mkcert() ]
 })
