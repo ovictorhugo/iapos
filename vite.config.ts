33baf450335e4d8346516f76544a12a90c67cd75
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2023-09-25 17:23:38 -0300

    1..0..0
    http://iapos.senaicimatec.com.br

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  server: { https: true },
+  server: { 
+    https: true,
+    host: 'http://iapos.senaicimatec.com.br'
+  },
   plugins: [ mkcert() ]
 })
