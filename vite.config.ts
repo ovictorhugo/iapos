c1cb135afd4dc1fb7f7bfb584ef1bfc6875815f0
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2023-09-26 15:45:22 -0300

    teste com cima

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  server: { 
-    https: true
-  },
-  plugins: [ mkcert() ]
+  plugins: [react()],
+
+  server: {
+    host: true,
+    port: 4000
+  }
 })
